0918db3c13a9596aabee0f2d6ce21fd9630f9683
Bart van Blokland <bartvblokl@gmail.com>
2014-12-21 14:18:06 +0100

    Sync commit

--- Relay/src/relay/types/ExpressionTokenType.java
+++ Relay/src/relay/types/ExpressionTokenType.java
@@ -5,9 +5,12 @@ public enum ExpressionTokenType {
 	ARGUMENT_LIST_CLOSE(new char[]{')'}),
 	DOT(new char[]{'.'}),
 	IDENTIFYER(new char[]{'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','_'}),
-	OPERATOR(new char[]{'+', '-', '*', '/'});
+	OPERATOR(new char[]{'+', '-', '*', '/'}),
+	NUMBER(new char[]{'1','2','3','4','5','6','7','8','9','0'});
 	
+	public final char[] applicableCharacters;
+
 	private ExpressionTokenType(char[] applicableCharacters) {
-		
+		this.applicableCharacters = applicableCharacters;
 	}
 }
