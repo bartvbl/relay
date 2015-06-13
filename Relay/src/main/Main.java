5431c5229d67280fc864fdfbe39ea8ca894d9a16
Bart (on TURBONINJA) <bartvblokl@gmail.com>
2015-06-13 16:35:51 +0200

    sync

--- Relay/src/main/Main.java
+++ Relay/src/main/Main.java
@@ -2,10 +2,12 @@ package main;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
 
+import parser.RelayParser;
 import relay.data.Block;
 import relay.parser.FileBuffer;
-import relay.parser.RelayParser;
+import relay.parser.RelaySymbolFactory;
 import relay.parser.errors.RelayParseException;
 import relay.renderer.TreeRenderer;
 import relay.tools.TreeVisualiser;
@@ -14,11 +16,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		try {
-			FileBuffer buffer = FileBuffer.open(new File("res/testfile.rl"));
-			Block root = RelayParser.parse(buffer);
+			File sourceFile = new File("res/testfile.rl");
 			System.out.println("Complete.");
 			//new TreeVisualiser(root);
-			new TreeRenderer(root);
+			Scanner scanner = new Scanner(sourceFile);
+			RelaySymbolFactory symbolFactory = new RelaySymbolFactory();
+			RelayParser parser = new RelayParser(scanner, symbolFactory);
 		} catch (IOException e) {
 			e.printStackTrace();
 		} catch (RelayParseException e) {
