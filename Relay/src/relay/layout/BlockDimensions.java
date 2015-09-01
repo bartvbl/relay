275cdec618eeb38c866f5da5b62e91103bc13010
Bart (on TURBONINJA) <bartvblokl@gmail.com>
2015-09-01 20:43:36 +0200

    Finished block specification

--- Relay/src/relay/layout/BlockDimensions.java
+++ Relay/src/relay/layout/BlockDimensions.java
@@ -13,6 +13,9 @@ public class BlockDimensions {
 	public final DimensionValue top;
 	public final DimensionValue height;
 	
+	public final DimensionDefinitionType horizontalDefinition;
+	public final DimensionDefinitionType verticalDefinition;
+	
 	public BlockDimensions(HashMap<String, BlockPropertyNode> propertyMap) {
 		this.left = createDimensionValue(DimensionValueType.left, propertyMap);
 		this.right = createDimensionValue(DimensionValueType.right, propertyMap);
@@ -21,6 +24,43 @@ public class BlockDimensions {
 		this.bottom = createDimensionValue(DimensionValueType.bottom, propertyMap);
 		this.top = createDimensionValue(DimensionValueType.top, propertyMap);
 		this.height = createDimensionValue(DimensionValueType.height, propertyMap);
+		
+		this.horizontalDefinition = determineHorizontalDefinitionType();
+		this.verticalDefinition = determineVerticalDefinitionType();
+	}
+
+	private DimensionDefinitionType determineHorizontalDefinitionType() {
+		if(left.isDefined && right.isDefined) {
+			return DimensionDefinitionType.LEFT_AND_RIGHT;
+		} else if(left.isDefined && width.isDefined) {
+			return DimensionDefinitionType.LEFT_AND_WIDTH;
+		} else if(right.isDefined && width.isDefined) {
+			return DimensionDefinitionType.RIGHT_AND_WIDTH;
+		} else if(left.isDefined) {
+			return DimensionDefinitionType.LEFT_ONLY;
+		} else if(right.isDefined) {
+			return DimensionDefinitionType.RIGHT_ONLY;
+		} else if(!left.isDefined && !width.isDefined && !height.isDefined) {
+			return DimensionDefinitionType.MATCH_PARENT;
+		}
+		throw new RuntimeException("Semantics error: size definition of width only is not allowed.");
+	}
+	
+	private DimensionDefinitionType determineVerticalDefinitionType() {
+		if(bottom.isDefined && top.isDefined) {
+			return DimensionDefinitionType.BOTTOM_AND_TOP;
+		} else if(bottom.isDefined && height.isDefined) {
+			return DimensionDefinitionType.BOTTOM_AND_HEIGHT;
+		} else if(top.isDefined && height.isDefined) {
+			return DimensionDefinitionType.TOP_AND_HEIGHT;
+		} else if(bottom.isDefined) {
+			return DimensionDefinitionType.BOTTOM_ONLY;
+		} else if(top.isDefined) {
+			return DimensionDefinitionType.TOP_ONLY;
+		} else if(!bottom.isDefined && !top.isDefined && !height.isDefined) {
+			return DimensionDefinitionType.MATCH_PARENT;
+		}
+		throw new RuntimeException("Semantics error: size definition of height only is not allowed.");
 	}
 
 	private DimensionValue createDimensionValue(DimensionValueType type, HashMap<String, BlockPropertyNode> propertyMap) {
--- Relay/src/relay/layout/DimensionDefinitionType.java
+++ Relay/src/relay/layout/DimensionDefinitionType.java
@@ -1,11 +1,19 @@
 package relay.layout;
 
 public enum DimensionDefinitionType {
+	LEFT_ONLY,
+	RIGHT_ONLY,
+	
 	LEFT_AND_RIGHT,
 	LEFT_AND_WIDTH,
 	RIGHT_AND_WIDTH,
 	
+	BOTTOM_ONLY,
+	TOP_ONLY,
+	
 	BOTTOM_AND_TOP,
 	BOTTOM_AND_HEIGHT,
-	TOP_AND_HEIGHT
+	TOP_AND_HEIGHT,
+	
+	MATCH_PARENT
 }
