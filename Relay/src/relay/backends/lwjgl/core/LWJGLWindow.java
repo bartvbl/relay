6708c7404bedfc383d35f46579adaf13c2b934dc
Bart (on TURBONINJA) <bartvblokl@gmail.com>
2016-03-17 19:27:51 +0100

    Moved handling of window dimensions into LWJGLWindow

--- Relay/src/relay/backends/lwjgl/core/LWJGLWindow.java
+++ Relay/src/relay/backends/lwjgl/core/LWJGLWindow.java
@@ -1,20 +1,30 @@
 package relay.backends.lwjgl.core;
 
-import org.lwjgl.LWJGLException;
+import java.util.concurrent.atomic.AtomicReference;
 
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+
+import lib.geom.Dimension2D;
+import lib.geom.Index2D;
 import lib.geom.IndexRectangle2D;
 import relay.Window;
 import relay.backends.lwjgl.infrastructure.LWJGLBackendThread;
 import relay.backends.lwjgl.rendering.RenderUtils;
+import relay.events.Event;
+import relay.events.EventType;
 import relay.layout.LayoutDefinition;
 
 public class LWJGLWindow extends Window {
 
-	private final LayoutDefinition layout;
+	public final LayoutDefinition layout;
+	private final AtomicReference<IndexRectangle2D> windowDimensions;
 
 	public LWJGLWindow(LayoutDefinition layout, String windowTitle, IndexRectangle2D windowDimensions) {
 		super(windowTitle, windowDimensions);
 		this.layout = layout;
+		this.windowDimensions = new AtomicReference<IndexRectangle2D>();
+		this.windowDimensions.set(dimensions);
 	}
 
 	@Override
@@ -25,6 +35,31 @@ public class LWJGLWindow extends Window {
 	@Override
 	protected void closeWindow() {
 		RenderUtils.destroyOpenGL();
+	}
+
+	public void updateDimensions() {
+		int windowWidth = Display.getWidth();
+		int windowHeight = Display.getHeight();
+		int windowX = Display.getX();
+		int windowY = Display.getY();
+		
+		IndexRectangle2D previousDimensions = windowDimensions.get();
+		
+		if(previousDimensions.width != windowWidth || previousDimensions.height != windowHeight) {
+			Dimension2D newWindowDimensions = new Dimension2D(windowWidth, windowHeight);
+			events.dispatchEvent(new Event<Dimension2D>(EventType.WINDOW_RESIZED, newWindowDimensions));
+		}
+		
+		if(previousDimensions.x1 != windowX || previousDimensions.y2 != windowY) { // Different because the origin of the rectangle is in the bottom left.
+			Index2D newWindowLocation = new Index2D(windowX, windowY);
+			events.dispatchEvent(new Event<Index2D>(EventType.WINDOW_MOVED, newWindowLocation));
+		}
+		
+		this.windowDimensions.set(new IndexRectangle2D(windowX, windowY, windowWidth, windowHeight));
+	}
+
+	public IndexRectangle2D getDimensions() {
+		return windowDimensions.get();
 	}
 
 }
--- Relay/src/relay/backends/lwjgl/infrastructure/LWJGLBackendThread.java
+++ Relay/src/relay/backends/lwjgl/infrastructure/LWJGLBackendThread.java
@@ -21,13 +21,10 @@ public class LWJGLBackendThread extends Thread {
 	
 	private final LWJGLWindow window;
 	private final String windowTitle;
-	private final AtomicReference<IndexRectangle2D> windowDimensions;
 
 	public LWJGLBackendThread(LWJGLWindow window, String windowTitle, IndexRectangle2D dimensions) {
 		this.window = window;
 		this.windowTitle = windowTitle;
-		this.windowDimensions = new AtomicReference<IndexRectangle2D>();
-		windowDimensions.set(dimensions);
 	}
 
 	public void run() {
@@ -42,35 +39,17 @@ public class LWJGLBackendThread extends Thread {
 	}
 
 	private void init() throws LWJGLException {
-		RenderUtils.initOpenGL(windowTitle, windowDimensions.get());
+		RenderUtils.initOpenGL(windowTitle, window.getDimensions());
 		RenderUtils.set2DMode();
 	}
 
 	private void mainLoop() {
 		while(!Display.isCloseRequested()) {
 			RenderUtils.newFrame();
-			updateWindowDimensions();
+			window.updateDimensions();
+			window.layout.update();
 		}
 	}
 
-	private void updateWindowDimensions() {
-		int windowWidth = Display.getWidth();
-		int windowHeight = Display.getHeight();
-		int windowX = Display.getX();
-		int windowY = Display.getY();
-		
-		IndexRectangle2D previousDimensions = windowDimensions.get();
-		
-		if(previousDimensions.width != windowWidth || previousDimensions.height != windowHeight) {
-			Dimension2D newWindowDimensions = new Dimension2D(windowWidth, windowHeight);
-			window.events.dispatchEvent(new Event<Dimension2D>(EventType.WINDOW_RESIZED, newWindowDimensions));
-		}
-		
-		if(previousDimensions.x1 != windowX || previousDimensions.y2 != windowY) { // Different because the origin of the rectangle is in the bottom left.
-			Index2D newWindowLocation = new Index2D(windowX, windowY);
-			window.events.dispatchEvent(new Event<Index2D>(EventType.WINDOW_MOVED, newWindowLocation));
-		}
-		
-		this.windowDimensions.set(new IndexRectangle2D(windowX, windowY, windowWidth, windowHeight));
-	}
+	
 }
